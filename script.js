343577e4fb002d09347aa7fb2153048e445bcb8e
Tino-687 <151873282+Tino-687@users.noreply.github.com>
2023-11-24 12:13:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function displayExampleSavings() {
     // Example static results
     const resultsHTML = `
         <h2>Estimated Savings</h2>
-        <p>Transition Cost: €25,000 (after incentives)</p>
+        <p>Transition Cost: €25,000 (without gouvernement incentives)</p>
         <p>Monthly Savings: €90</p>
         <p>Payback Period: 7.5 years</p>
     `;
