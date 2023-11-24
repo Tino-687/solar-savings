f3d36bcaaf00cbe7649c9e9bdefb05f5af81c4b1
Tino-687 <151873282+Tino-687@users.noreply.github.com>
2023-11-24 21:44:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function displayExampleSavings() {
-    // Assuming the user enters their monthly bill amount in the billInput field
+
     const monthlyBill = document.getElementById('billInput').value;
-    // Remove non-numeric characters and convert to number
+
     const numericBill = parseFloat(monthlyBill.replace(/[^\d.]/g, ''));
 
-    // Calculate the yearly consumption
-    // Here, we assume 0.5 euro = 1 kWh
     const yearlyConsumption = (numericBill / 0.5) * 12;
 
-    // Example static results with updated values
+   
     const resultsHTML = `
         <h2>Estimated Savings</h2>
         <p>Transition Cost: €25,000 (without gouvernement incentives)</p>
