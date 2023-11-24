a70058f693d3f3d4f619edd381287ca5d08bb639
Tino-687 <151873282+Tino-687@users.noreply.github.com>
2023-11-24 12:00:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function displayExampleSavings() {
+    // Example static results
+    const resultsHTML = `
+        <h2>Estimated Savings</h2>
+        <p>Transition Cost: €25,000 (after incentives)</p>
+        <p>Monthly Savings: €90</p>
+        <p>Payback Period: 7.5 years</p>
+    `;
+
+    document.getElementById('results').innerHTML = resultsHTML;
+}
