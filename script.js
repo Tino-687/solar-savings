0636f44ce73b9365036ded2794024db032fa040b
Tino-687 <151873282+Tino-687@users.noreply.github.com>
2023-11-24 21:34:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 function displayExampleSavings() {
-    // Example static results with updated values
-    const resultsHTML = `
-        <h2>Estimated Savings</h2>
-        <p>Transition Cost: €25,000 (after incentives)</p>
-        <p>Monthly Savings: €90</p>
-        <p>Payback Period: 5 years</p>
-    `;
-
-    document.getElementById('results').innerHTML = resultsHTML;
-}
-function displayExampleSavings() {
     // Assuming the user enters their monthly bill amount in the billInput field
     const monthlyBill = document.getElementById('billInput').value;
     // Remove non-numeric characters and convert to number
@@ -22,14 +11,27 @@ function displayExampleSavings() {
     // Example static results with updated values
     const resultsHTML = `
         <h2>Estimated Savings</h2>
-        <p>Transition Cost: €25,000 (after incentives)</p>
-        <p>Monthly Savings: €90</p>
+        <p>Transition Cost: €25,000 (without gouvernement incentives)</p>
+        <p>Monthly Savings: €40</p>
         <p>Payback Period: 5 years</p>
         <div class='info-bubble'>
-            You consume on average ${yearlyConsumption.toFixed(2)} kWh per month according to your electricity bill. 
-            Keep in mind that  the best way to cut energy costs is to cut down on usage!
+            You consume on average ${yearlyConsumption.toFixed(2)} kWh per year according to your electricity bill. 
+            Remember that the best way to cut energy costs is to cut down on usage!
         </div>
     `;
 
     document.getElementById('results').innerHTML = resultsHTML;
+    // Show the email section
+    document.getElementById('emailSection').style.display = 'block';
+}
+
+function sendQuoteRequest() {
+    const email = document.getElementById('emailInput').value;
+    if (email) {
+        // Here you would add logic to send the email to your server
+        // For demonstration, we'll just show an alert
+        alert("Quote request sent! We will contact you at " + email);
+    } else {
+        alert("Please enter a valid email address.");
+    }
 }
